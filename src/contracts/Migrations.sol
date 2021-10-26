f49263fb44dc62024002888c349c8b320c9940b9
Thibault Benedito <thibaultbenedito@gmail.com>
2021-10-26 17:35:05 +0200

    added some folder and the first contract

---
+++ src/contracts/Migrations.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract Migrations {
+    address public owner;
+    uint public last_completed_migration;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier restricted() {
+        if (msg.sender == owner) _;
+    }
+
+    function setCompleted(uint _completed) public restricted {
+        last_completed_migration = _completed;
+    }
+    
+    function upgrade(address _new_address) public restricted {
+        Migrations upgraded = Migrations(_new_address);
+        upgraded.setCompleted(last_completed_migration);
+    }
+}
