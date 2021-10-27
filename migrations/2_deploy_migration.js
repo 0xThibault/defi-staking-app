512d890489a5af0a6dd03dd12ccdedac5b3e8011
Thibault Benedito <thibaultbenedito@gmail.com>
2021-10-27 18:21:14 +0200

    fixed some issues

--- migrations/2_deploy_migration.js
+++ migrations/2_deploy_migration.js
@@ -1,6 +1,24 @@
-// eslint-disable-next-line no-undef
+/* eslint-disable no-undef */
 const Tether = artifacts.require("Tether");
+const RWD = artifacts.require("RWD");
+const DecentralBank = artifacts.require("DecentralBank");
 
-module.exports = async function (deployer) {
-    deployer.deploy(Tether);
+module.exports = async function (deployer, network, accounts) {
+    // Deploy Tether contract
+    await deployer.deploy(Tether);
+    const tether = await Tether.deployed()
+
+    // Deploy RWD contract
+    await deployer.deploy(RWD);
+    const rwd = await RWD.deployed();
+
+    // Deploy DecentralBank contract
+    await deployer.deploy(DecentralBank, rwd.address, tether.address);
+    const decentralBank = await DecentralBank.deployed();
+
+    // Transfer all RWD tokens to Decentral Bank
+    await rwd.transfer(decentralBank.address, "1000000000000000000000000");
+
+    // Transfer 100 Tether token to investor
+    await tether.transfer(accounts[1], "100000000000000000000");
 };
